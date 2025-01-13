b8eeb3be710d83e5762e91e41250e2c701eeeaa0
James SANCHEZ <james.sanchez@laplateforme.io>
2025-01-13 11:19:17 +0100

    job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,23 @@
+<?php
+$str = "LaPlateforme";
+$str2 = "Vive";
+$str3 = "!";
+
+echo " $str2" . " $str". " $str3";
+echo "<br><br>";
+
+$val = 6;
+
+echo $val + 4;
+echo "<br>";
+
+$myBool = False;
+
+echo $myBool;
+echo "<br>";
+
+$myBool = True;
+echo $myBool;
+
+?>
+
