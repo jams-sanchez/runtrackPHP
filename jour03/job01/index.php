7d9ffe34cb5f70c42f301e638b6b227099e6e38e
James SANCHEZ <james.sanchez@laplateforme.io>
2025-01-15 10:56:52 +0100

    Créez une string nommée str et affectez-y : I'm sorry Dave I'm afraid I can't do that. Créez un algorithme qui parcourt cette chaine et affiche uniquement les voyelles.

--- jour03/job01/index.php
+++ jour03/job01/index.php
@@ -2,7 +2,7 @@
 
 $tableau = array(200, 204, 173, 98, 171, 404, 459);
 
-foreach ($tableau as $index => $num) {
+foreach ($tableau as $num) {
     if ($num % 2 == 0) {
         echo $num . " est pair <br>";
     } else {
---
+++ jour03/job03/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$str = "I'm sorry Dave I'm afraid I can't do that";
+$str = strtolower($str);
+
+$strToTab = str_split($str);
+
+foreach ($strToTab as $key => $value) {
+    if ($value == 'a' or $value == 'e' or $value == "i" or $value == "o" or $value == "u" or $value == "y") {
+        echo $value . " ";
+    }
+}
