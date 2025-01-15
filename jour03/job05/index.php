5ca9c47673f8f23768110708e820c7ff64ea8918
James SANCHEZ <james.sanchez@laplateforme.io>
2025-01-15 14:23:41 +0100

    jour3 job05 Créez un algorithme qui parcourt, compte et stocke le nombre d'occurrences de consonnes et de voyelles de str. Affichez ces résultats dans un tableau html qui a comme thead Voyelles et Consonnes.

---
+++ jour03/job05/index.php
@@ -0,0 +1,77 @@
+<?php
+
+$str = "On n'est pas le meilleur quand on le croit mais quand on le sait";
+$str = strtolower($str);
+$strToTab = str_split($str);
+
+$countVoy = 0;
+$countCon = 0;
+
+$dic = array(
+    'voyelles' => array(),
+    'consonnes' => array(),
+    'caracteres' => array()
+);
+
+foreach ($strToTab as $value) {
+
+    if ($value == " " or $value == "'") {
+        array_push($dic['caracteres'], $value);
+    } elseif ($value == 'a' or $value == 'e' or $value == "i" or $value == "o" or $value == "u" or $value == "y") {
+        array_push($dic['voyelles'], $value);
+        $countVoy++;
+    } else {
+        array_push($dic['consonnes'], $value);
+        $countCon++;
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tableau</title>
+</head>
+
+<body>
+
+    <table>
+        <tr>
+            <td></td>
+            <th>Voyelles</th>
+            <th>Consonnes</th>
+        </tr>
+        <tr>
+            <th>Nombres</th>
+            <td><?php echo $countVoy; ?></td>
+            <td><?php echo $countCon; ?></td>
+        </tr>
+        <tr>
+            <th>Lettres</th>
+            <td>
+                <?php
+                for ($i = 0; $i < count($dic['voyelles']); $i++) {
+                    echo ($dic['voyelles'][$i]);
+                }
+                ?>
+            </td>
+            <td>
+                <?php
+                for ($i = 0; $i < count($dic['consonnes']); $i++) {
+                    echo ($dic['consonnes'][$i]);
+                }
+                ?>
+            </td>
+        </tr>
+
+
+    </table>
+
+
+</body>
+
+</html>
