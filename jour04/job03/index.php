69db6fdf11f604b2b97d2c56653755303a68646b
James SANCHEZ <james.sanchez@laplateforme.io>
2025-01-16 12:05:33 +0100

    Jour04 job03 Développez un algorithme qui affiche le nombre d’arguments .

---
+++ jour04/job03/index.php
@@ -0,0 +1,42 @@
+<?php
+
+$count = 0;
+
+foreach ($_POST as $key => $value) {
+    $count++;
+}
+
+echo $count;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>formulaire</title>
+</head>
+
+<body>
+
+    <form action="index.php" method="post">
+        <p>
+            <label for="nom">Nom:</label>
+            <input type="text" name="nom" />
+        </p>
+        <p>
+            <label for="prenom">Prénom:</label>
+            <input type="text" name="prenom" />
+        </p>
+        <p>
+            <label for="age">Age:</label>
+            <input type="text" name="age" />
+        </p>
+        <button type="submit">Envoyer</button>
+    </form>
+
+</body>
+
+</html>
