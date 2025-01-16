8909cb4b05d5f2ca7d55bbd8f674c15745ba6c1e
James SANCHEZ <james.sanchez@laplateforme.io>
2025-01-16 11:37:51 +0100

    Jour04 job02 Créez un algorithme qui affiche dans un tableau HTML l’ensemble des arguments  et les valeurs associées.

---
+++ jour04/job02/index.php
@@ -0,0 +1,63 @@
+<?php
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tableau</title>
+</head>
+
+<body>
+
+    <form action="index.php" method="get">
+        <p>
+            <label for="nom">Nom:</label>
+            <input type="text" name="nom" />
+        </p>
+        <p>
+            <label for="prenom">Prénom:</label>
+            <input type="text" name="prenom" />
+        </p>
+        <p>
+            <label for="age">Age:</label>
+            <input type="text" name="age" />
+        </p>
+        <button type="submit">Envoyer</button>
+    </form>
+
+
+    <table>
+        <tr>
+            <th>Arguments</th>
+            <th>Valeurs</th>
+        </tr>
+        <tr>
+            <td>
+                <?php
+                foreach ($_GET as $key => $value) {
+                    echo $key . "<br>";
+                }
+                ?>
+            </td>
+            <td>
+                <?php
+                foreach ($_GET as $value) {
+                    echo $value . "<br>";
+                }
+                ?>
+            </td>
+        </tr>
+    </table>
+
+
+
+</body>
+
+</html>
