909b1ccf8767b66adb6158d4b4c3ad0c4c039634
James Sanchez <james.sanchez@laplateforme.io>
2025-01-17 10:46:04 +0100

    Jour04 job07 Créez un formulaire avec deux entrées largeur et hauteur. Vous devez créer un algorithme qui affiche, à la validation du formulaire, une maison telle que : Si on entre les valeurs : largeur = 10 et hauteur =5 dans les champs, la maison qui s’affiche doit ressembler à l’exemple fourni. ---- Non concluant

---
+++ jour04/job07/index.php
@@ -0,0 +1,42 @@
+<?php
+
+if (count($_GET) != 0) {
+
+    $haut = $_GET["hauteur"];
+    $larg = $_GET["largeur"];
+    $tot = $larg / $haut * 2;
+
+
+    for ($j = 0; $j < $haut; $j++) {
+        echo str_repeat(".", $tot-- * 2) . "/" . str_repeat("_", $j * 2) . "\\ <br>";
+    }
+
+    for ($i = 0; $i < $haut; $i++) {
+        echo "|" . str_repeat("_", $larg) . "| <br>";
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>maison</title>
+</head>
+
+<body>
+
+    <form action="index.php" method="$_GET">
+        <label>hauteur</label>
+        <input type="text" name="hauteur">
+        <label>largeur</label>
+        <input type="text" name="largeur">
+        <button type="submit">Envoyer</button>
+    </form>
+
+</body>
+
+</html>
