a193ae4ba02874db54b749546aa5d9454b11293d
James Sanchez <james.sanchez@laplateforme.io>
2025-01-21 13:42:46 +0100

    jour07 job03 Créer un formulaire qui contient un input de type text et un bouton. Lorsque l'on valide le formulaire, le contenu est ajouté dans une variable de session. Afficher l'ensemble des prénoms. Ajouter un bouton reset qui permet de réinitialiser la liste.

---
+++ jour07/job03/index.php
@@ -0,0 +1,44 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION["listePrenom"])) {
+    $_SESSION["listePrenom"] = "";
+}
+
+if (isset($_SESSION["listePrenom"]) and isset($_POST["envoyer"])) {
+    $_SESSION["listePrenom"] .= $_POST["prenom"] . "<br>";
+}
+
+if (isset($_POST["reset"])) {
+    $_SESSION["listePrenom"] = "";
+    session_destroy();
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire</title>
+</head>
+
+<body>
+    <form action="index.php" method="post">
+        <label>Entrer votre prénom : </label>
+        <input type="text" name="prenom" />
+        <button type="submit" name="envoyer">Envoyer</button>
+        <button type="submit" name="reset">Reset</button>
+    </form>
+
+    <p>
+        Liste des prenoms :
+        <?= "<br>" . $_SESSION["listePrenom"]; ?>
+    <p>
+</body>
+
+</html>
